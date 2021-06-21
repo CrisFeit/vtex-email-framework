215f38db19177519a0446800c223ee02d5ad79a7
crfeitoza <crfeitoza@rd.com.br>
2021-06-21 00:57:35 -0300

    feat: email stylelint

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,21 +1,25 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
 
-  devServer:{
+  devServer: {
     hotOnly: true,
     liveReload: false,
     open: true,
     watchOptions: {
       ignored: /node_modules/
-    }  
+    }
   },
 
   plugins: [
     new HtmlWebpackPlugin(),
+    new StylelintPlugin({
+      fix: true,
+    })
   ],
 
   module: {
