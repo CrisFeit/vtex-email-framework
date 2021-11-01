39345d6ecc31bd73cea6256a97303feee94a379c
crisfeit <crisrfab@gmail.com>
2021-11-01 17:54:48 -0300

    chore: add projects to  monorepo

---
+++ packages/vtex-email-template/__tests__/render.test.js
@@ -0,0 +1,36 @@
+
+
+// ------------------------- Don't change this file ---------------------
+
+const { readdirSync } = require('fs');
+const { resolve } = require('path');
+const files = resolve('emails', 'dist')
+const app = require('vtex-email-framework/server/app')
+const request = require('isomorphic-fetch')
+const port = 3050
+
+app.set('views', files)
+const server = app.listen(port)
+const templates = readdirSync(files)
+  .filter(file => /.hbs$/gi.test(file))
+  .map(file => file.split('.hbs')[0])
+
+afterAll(() => server.close())
+
+describe("dist/", () => {
+
+  for (let template of templates) {
+    test(`${template}`, async () => {
+
+      const response = await request(`http://localhost:${port}/${template}`)
+
+      if (response.status !== 200) {
+        const message = await response.text()
+        throw new Error(`${message.split('<pre>')[1].split('<br>')[0].replace(/&#39;/g, "'")}`)
+      }
+
+      expect(response.status).toBe(200)
+    });
+  }
+
+});
